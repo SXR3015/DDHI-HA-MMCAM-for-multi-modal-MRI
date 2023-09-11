5374074c7fa08e3d6733eb34cf616f0265857b9b
SXR3015 <120301250+SXR3015@users.noreply.github.com>
2023-09-11 15:34:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -39,9 +39,6 @@ def test_epoch(epoch, data_loader, model, criterion, opt, logger):
         labels = list(map(int,labels))
         inputs = (torch.unsqueeze(input, 1) for input in inputs)
         inputs = (input.type(torch.FloatTensor) for input in inputs)
-        # inputs = torch.unsqueeze(inputs,1)
-        # inputs = inputs.type(torch.FloatTensor)
-
         if not opt.no_cuda:
             labels = torch.LongTensor(labels).cuda(non_blocking = True)
         with torch.no_grad():
@@ -55,11 +52,7 @@ def test_epoch(epoch, data_loader, model, criterion, opt, logger):
             inputs_1 = (inputs[index] for index in indexs)
             inputs = [list(inputs_1), labels]
             loss, outputs = model(inputs)
-            #module.Resnet.layer4
-            # cam = ScoreCAM(model_val, ['module.Resnet.layer4','module.dfc_pyramid.conv_last_2','module.Resnet.layer4','module.Resnet.layer4'], batch_size=2,
-            #        input_shape=[[inputs[0][0].shape[1:],inputs[0][1].shape[1:],inputs[0][2].shape[1:],inputs[0][3].shape[1:]],inputs[1].shape])
-            # cam = ScoreCAM(model_val, ['module.Resnet.layer4','module.dfc_pyramid.conv_last_2','module.Resnet.layer4','module.Resnet.layer4'], batch_size=2,
-            #        input_shape=[[inputs[0][0].shape[1:],inputs[0][1].shape[1:],inputs[0][2].shape[1:],inputs[0][3].shape[1:]],inputs[1].shape])
+
             cam = ScoreCAM(model_val, ['module.Resnet.layer4','module.dfc_pyramid.dfc_encoder_40','module.Resnet.layer4',
                                        'module.Resnet.layer4'], batch_size=2,
                    input_shape=[[inputs[0][0].shape[1:],inputs[0][1].shape[1:],inputs[0][2].shape[1:],inputs[0][3].shape[1:]],inputs[1].shape])
@@ -70,17 +63,10 @@ def test_epoch(epoch, data_loader, model, criterion, opt, logger):
             alff_arr = ((Map[0][1, ...].squeeze().cpu() + Map[0][0, ...].squeeze().cpu())/2 + alff_arr.cpu())/2
             dfc_arr = ((Map[1][1, ...].squeeze().cpu() + Map[1][0, ...].squeeze().cpu())/2 + dfc_arr.cpu())/2
             fa_arr =  ((Map[3][1, ...].squeeze().cpu() + Map[3][0, ...].squeeze().cpu())/2 + fa_arr.cpu())/2
-            # scipy.io.savemat('Fc_whole.mat', {'test': fc_arr})
             '''
             save the activation map in multi-modal MRI
             '''
-            # np.save(r'.\alff_whole.npy',alff_arr)
             np.save(r'.\dfc40_whole.npy', dfc_arr)
-            # np.save(r'.\fc_whole.npy', fc_arr)
-            # np.save(r'.\fa_whole.npy', fa_arr)
-            # inputs = Variable(inputs)
-            # outputs = model(inputs)
-            #loss = criterion(outputs, labels)
             acc = calculate_accuracy(outputs, labels)
             recall, precision, f1, sensitivity, specificity = calculate_recall(outputs, labels, opt)
 
@@ -156,68 +142,3 @@ if opt.test:
     model.load_state_dict(checkpoint['state_dict'])
 
     test_epoch(1, test_loader, model, criterion, opt, test_logger)
-
-# def calculate_test_results(output_buffer,sample_id,test_results,labels):
-#     outputs =torch.stack(output_buffer)
-#     average_score = torch.mean(outputs,dim=0)
-#     sorted_scores,locs = torch.topk(average_score,k=1)
-#     results=[]
-#     for i in range(sorted_scores.size(0)):
-#         score = copy.deepcopy(sorted_scores[i])
-#         if isinstance(score, torch.Tensor):
-#             score = score.data.cpu().numpy()
-#             score = score.item()
-#         results.append({
-#             'label':labels[i],
-#             'score':score
-#         })
-#     test_results['results'][sample_id] = results
-#
-# def test(data_loader, model, opt, labels):
-#     print('test')
-#
-#     model.eval()
-#
-#     batch_time = AverageMeter()
-#     data_time = AverageMeter()
-#
-#     end_time = time.time()
-#     output_buffer = []
-#     sample_id = ''
-#     test_results = {'results': {}}
-#     with torch.no_grad():
-#         for i, (inputs, targets) in enumerate(data_loader):
-#             data_time.update(time.time() - end_time)
-#
-#             inputs = torch.unsqueeze(inputs, 1)  # 在 1 的位置加一个维度
-#             inputs = Variable(inputs)
-#             outputs = model(inputs)
-#             # if not opt.no_softmax_in_test:
-#             #outputs = F.softmax(outputs)
-#
-#             for j in range(outputs.size(0)):
-#                 if not (i == 0 and j == 0):
-#                     calculate_test_results(output_buffer, sample_id, test_results, labels)
-#                     output_buffer = []
-#                 output_buffer.append(outputs[j].data.cpu())
-#                 sample_id = labels[j]
-#             if (i % 100) == 0:
-#                 with open(
-#                         OsJoin(opt.result_path, '{}.json'.format(
-#                             opt.test_subset)), 'w') as f:
-#                     json.dump(test_results, f)
-#
-#             batch_time.update(time.time() - end_time)
-#             end_time = time.time()
-#
-#             print('[{}/{}]\t'
-#                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-#                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'.format(
-#                       i + 1,
-#                       len(data_loader),
-#                       batch_time=batch_time,
-#                       data_time=data_time))
-#     with open(
-#             OsJoin(opt.result_path, opt.data_type, opt.model_name, str(opt.model_depth), '{}.json'.format(opt.test_subset)),
-#             'w') as f:
-#         json.dump(test_results, f)
